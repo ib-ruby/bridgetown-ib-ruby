0e2773df462fb99bf9445bc4ec995e8a6a425e32
Hartmut Bischoff <topofocus@gmaiol.com>
2023-01-10 16:23:07 +0000

    Include logger in gateway call

--- lib/bridgetown-ib-ruby/initializer.rb
+++ lib/bridgetown-ib-ruby/initializer.rb
@@ -10,18 +10,17 @@ module BridgetownIbRuby
 
   def self.gateway_configuration(config)
     # Reads a YAML configuration file, evaluating any ERB, then parsing the resulting YAML.
-    ActiveSupport::ConfigurationFile.parse(File.join(config.root_dir, "config", "tws.yml"))
+    ActiveSupport::ConfigurationFile.parse(File.join(config.root_dir, "config", "tws.yml")).merge( logger: log_writer )
   end
 
-    #
-  end
   def self.log_writer
     Bridgetown::LogWriter.new #.tap(&:enable_prefix)
   end
-
+end
 Bridgetown.initializer :"bridgetown-ib-ruby" do | config |
    IB::Gateway.new( **BridgetownIbRuby.gateway_configuration( config ) ) do |gw|
     # to do: subscribe to news , too
   gw.tws.subscribe( :Alert, :ManagedAccounts ){ | msg | gw.tws.logger.info  msg.to_human  }
+  gw.logger.level =  Logger::INFO
   end
 end
